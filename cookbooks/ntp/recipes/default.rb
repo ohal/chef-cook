3e36d55941982b9420c12234f9cc973e731da3ac
root <root@cchefcli.(none)>
2013-04-09 15:28:28 +0300

    first commit

--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -6,3 +6,13 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package 'ntp'
+
+template '/etc/ntp.conf' do
+  source    'ntp.conf.erb'
+  notifies  :restart, 'service[ntpd]'
+end
+
+service 'ntpd' do
+  action [:enable, :start]
+end
